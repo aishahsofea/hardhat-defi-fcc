3e13beba9fea1218fa0bbdd2c5d30ae45bc46dbf
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-08-13 13:55:31 +0800

    write script for borrowing

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,7 @@ require("dotenv").config();
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -16,6 +17,9 @@ module.exports = {
     hardhat: {
       chainId: 31337,
       blockConfirmations: 1,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
     },
     rinkeby: {
       chainId: 4,
@@ -40,6 +44,7 @@ module.exports = {
     compilers: [
       { version: "0.8.7" },
       { version: "0.6.6" },
+      { version: "0.6.12" },
       { version: "0.4.19" },
     ],
   },
